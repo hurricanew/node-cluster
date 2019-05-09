fc260661abe177832d87fbb61f9c60811b7dd095
hurricanew <hurricanew@hotmail.com>
2019-05-09 21:18:29 +1000

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# node-cluster
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,12 @@
+var http = require("http");
+const pid = process.pid;
+
+//create a server object:
+http
+  .createServer(function(req, res) {
+    for(let i=0; i<1e7; i++)
+    res.end(`handle by process ${pid}`); //end the response
+  })
+  .listen(8080,()=>{
+    console.log(`started process ${pid}`);
+  }); //the server object listens on port 8080
