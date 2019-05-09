69835bd7e8e2994bb89d6acd3c88b104905b9e3d
hurricanew <hurricanew@hotmail.com>
2019-05-09 23:12:23 +1000

    node cluster user count broadcast
    https://codesandbox.io/s/nrp93m1v1l

--- src/cluster.js
+++ src/cluster.js
@@ -1,16 +1,29 @@
 const cluster = require("cluster");
 const os = require("os");
-
+const numOfUsersInDB = function ()  {
+  this.count = this.count || 5;
+  this.count = this.count * this.count;
+  return this.count;
+};
 if (cluster.isMaster) {
   const cpus = os.cpus().length;
   console.log(`forking for ${cpus} cpus`);
   for (var i = 0; i < cpus; i++) {
     cluster.fork();
   }
+
+  const updateWorkers = () => {
+    const userCount = numOfUsersInDB();
+    Object.values(cluster.workers).forEach(worker => {
+      worker.send({ userCount });
+    });
+  };
+  updateWorkers();
+  setInterval(updateWorkers, 10000);
   // require('./index');
-  Object.values(cluster.workers).forEach(worker => {
-    worker.send(` hello worker ${worker.id}`);
-  });
+  //   Object.values(cluster.workers).forEach(worker => {
+  //     worker.send(` hello worker ${worker.id}`);
+  //   });
 } else {
   require("./index");
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 var http = require("http");
 const pid = process.pid;
-
+let userCount
 //create a server object:
 http
   .createServer(function(req, res) {
     for(let i=0; i<1e7; i++)
-    res.end(`handle by process ${pid}`); //end the response
+    // res.write(`handle by process ${pid}`); //end the response
+    res.end(`handle by process ${pid} \n users ${userCount}`);
   })
   .listen(8080,()=>{
     console.log(`started process ${pid}`);
   }); //the server object listens on port 8080
 process.on('message',(msg)=>{
-  console.log(`message from master ${msg}`)
+  userCount=msg.userCount;
 
 })
