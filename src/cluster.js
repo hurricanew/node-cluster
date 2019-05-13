75454298938e8603568bfab0d795065132f4fb88
hurricanew <hurricanew@hotmail.com>
2019-05-13 19:54:40 +1000

    crush and restart
    https://codesandbox.io/s/lrpnrwww0z

--- src/cluster.js
+++ src/cluster.js
@@ -1,13 +1,19 @@
 const cluster = require("cluster");
 const os = require("os");
 
-if(cluster.isMaster){
-    const cpus = os.cpus().length;
-    console.log(`forking for ${cpus} cpus`);
-    for(var i=0;i<cpus;i++){
-        cluster.fork();
+if (cluster.isMaster) {
+  const cpus = os.cpus().length;
+  console.log(`forking for ${cpus} cpus`);
+  for (var i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code) => {
+    if (code !== 0 && !worker.exitedAfterDisconnect) {
+      console.log(`worker ${worker.id} crashed, start new`);
+      cluster.fork();
     }
-    // require('./index');
-}else{
-    require('./index');
+  });
+  // require('./index');
+} else {
+  require("./index");
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const pid = process.pid;
 //create a server object:
 http
   .createServer(function(req, res) {
-    for(let i=0; i<1e7; i++)
-    res.end(`handle by process ${pid}`); //end the response
+    for (let i = 0; i < 1e7; i++) res.end(`handle by process ${pid}`); //end the response
   })
-  .listen(8080,()=>{
+  .listen(8080, () => {
     console.log(`started process ${pid}`);
   }); //the server object listens on port 8080
+//random crush
+setTimeout(() => {
+  process.exit(1);
+}, Math.random() * 10000);
